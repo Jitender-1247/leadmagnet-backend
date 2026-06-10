2a4a9857a42931cd85c93a25305aed44fc1ba5ee
Jitender <194709697+Jitender-1247@users.noreply.github.com>
2026-06-10 12:29:07 +0530

    //

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ const bcrypt = require('bcrypt');
 const cookieOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: 'none',
+    sameSite: false,
     maxAge: 7 * 24 * 60 * 60 * 1000
 };
 
